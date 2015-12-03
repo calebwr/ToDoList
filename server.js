60a295e80598d2887476d0495cea54ab9b52c282
Caleb <cawright@ucollege.edu>
2015-12-03 05:20:37 +0000

    added to layout and index

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ var express = require('express');
 
 var app = express();
 app.set('view engine', 'hbs');
+app.use(express.static('public'));
 
+app.locals.sitename = 'To Do List:';
+
+var toDoList = [];
+
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: "To Do's",
+    items: toDoList
+  });
+});
 
 
 app.listen(8080);
